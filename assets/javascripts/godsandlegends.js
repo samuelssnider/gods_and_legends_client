a0ec82da91d43d6212109cbffb6ca6e6b52bb6cd
samuelssnider <sam_redins@yahoo.com>
2017-11-13 09:18:26 -0700

    first request (index) up and running

--- assets/javascripts/godsandlegends.js
+++ assets/javascripts/godsandlegends.js
@@ -5,15 +5,21 @@ $(document).ready(function(){
   
   $('form').on('submit', function(event){
     event.preventDefault();
-  })
+  });
+  
+  var handleError = function(){
+    $('#latest-posts').append('<p class="post">Something went wrong. Try again later</p>');
+  }
   
   var getAllDomains = function() {
     return $.ajax({
       url: API + '/api/v1/domains',
       method: 'GET',
     }).done(function(data) {
+      debugger
+      console.log(data);
       for(i = 0; data.length; i++) {
-        $('#latest-posts').append('<p class="post">' + data[i].name + '</p>');
+        $('#latest-domains').append('<p class="domain">' + data[i].name + '</p>');
       }
     }).fail(function() {
       handleError();
