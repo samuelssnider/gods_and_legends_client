cec97a70d4a64821d87bb4d2719cce592fdcf8dc
samuelssnider <sam_redins@yahoo.com>
2017-11-13 09:45:19 -0700

    second request (show) up and running, need to add create, edit and delete to api app in order to complete the AJAX for those requests

--- assets/javascripts/godsandlegends.js
+++ assets/javascripts/godsandlegends.js
@@ -16,8 +16,6 @@ $(document).ready(function(){
       url: API + '/api/v1/domains',
       method: 'GET',
     }).done(function(data) {
-      debugger
-      console.log(data);
       for(i = 0; data.length; i++) {
         $('#latest-domains').append('<p class="domain">' + data[i].name + '</p>');
       }
@@ -26,5 +24,20 @@ $(document).ready(function(){
     })
   }
   
+  var getSingleDomain = function() {
+    var domainId = $(".show-form input[name='show-id']").val();
+    return $.ajax({
+      url: API + '/api/v1/domains/' + domainId,
+      method: 'GET',
+    }).done(function(data) {
+      console.log(data);
+      $('#latest-domains').append('<p class="domain">' + data.name + '<p>');
+    }).fail(function() {
+      handleError();
+    });
+  }
+  
+  
   $('button[name="button-fetch"]').on('click', getAllDomains);
+  $(".show-form input[type='submit']").on('click', getSingleDomain);
 });
