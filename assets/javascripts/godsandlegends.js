e2b29c02e816a80004d3062f8f14df84a55146bb
samuelssnider <sam_redins@yahoo.com>
2017-11-13 18:07:12 -0700

    update working, but throwing error

--- assets/javascripts/godsandlegends.js
+++ assets/javascripts/godsandlegends.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
   });
   
   var handleError = function(){
-    console.log(API);
     $('#latest-domains').append('<p class="domain">Something went wrong. Try again later</p>');
   }
   
@@ -51,10 +50,38 @@ $(document).ready(function(){
     })
   }
   
+  var updateDomain = function() {
+    var updateDomainId = $(".update-form input[name='update-id']").val();
+    var updateDomainDescription = $(".update-form input[name='domain-description']").val();
+    return $.ajax({
+      url: API + '/api/v1/domains/' + updateDomainId,
+      method: 'PUT',
+      data: { name: updateDomainDescription },
+    }).done(function(data) {
+      $('#latest-domains').append('<p class="domain">Your Domain with id '+ updateDomainId +' has been updated</p>');
+    }).fail(function() {
+      handleError();
+    })
+  }
+  
+  var deleteDomain = function() {
+    var deleteId = $(".delete-form input[name='delete-id']").val();
+    return $.ajax({
+      url: API + '/api/v1/domains/' + deleteId,
+      method: 'DELETE',
+    }).done(function(data) {
+      $('#latest-domains').append('<p class="domain">Your Domain with id '+ delete +' has been delteted</p>');
+    }).fail(function() {
+      handleError();
+    })
+  }
+  
+  
   
   $('button[name="button-fetch"]').on('click', getAllDomains);
   $(".show-form input[type='submit']").on('click', getSingleDomain);
   $(".post-form input[type='submit']").on('click', createNewDomain);
+  $(".update-form input[type='submit']").on('click', updateDomain);
+  $(".delete-form input[type='submit']").on('click', deleteDomain);
 
-  
 });
